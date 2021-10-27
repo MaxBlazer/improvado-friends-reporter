26db945d915e4490d3cbbfae142be6261f1f447a
Сапожников Максим Николаевич <sapozhnikov_mn@lemz-t.ru>
2021-10-27 13:02:16 +0700

    first draft

--- improvado_friend_reporter/main.py
+++ improvado_friend_reporter/main.py
@@ -1,6 +1,14 @@
+import requests
+import json
+
 
 def main():
-    print('hello, world!')
+    access_token = input('Enter access token: ')
+    url = f'https://api.vk.com/method/friends.get?v=5.81&access_token={access_token}'
+    response = requests.get(url)
+    friend_ids = json.loads(response.text)['response']['items']
+    print(friend_ids)
+
 
 if __name__ == '__main__':
     main()
