8a7690b5e23bb8c2864a838b1eeeb3717d8a4b0e
Сапожников Максим Николаевич <sapozhnikov_mn@lemz-t.ru>
2021-10-27 15:04:28 +0700

    more prompt added

--- improvado_friend_reporter/main.py
+++ improvado_friend_reporter/main.py
@@ -56,16 +56,28 @@ def dump_report(data, path='report', kind='csv'):
 
 def main():
     access_token = input('Enter access token: ')
+
     user_id = input('Enter user id: ')
+    if not user_id:
+        user_id = 0
+        print(f"User id {user_id} is chosen")
+
+    save_path = input('Enter file save path: ')
+    if not save_path:
+        save_path = 'report'
+        print(f"File will be saved to '{save_path}' file")
+
+    file_format = input('Choose file format (possible values: csv, tsv, json): ')
+    if not file_format:
+        file_format = 'csv'
+        print(f"File format {file_format} is chosen")
+
     api = VK(api_version, access_token)
 
     friend_ids = api.get_friends(user_id)['response']['items']
-    print(friend_ids)
     friends = api.get_users(friend_ids, fields=['city', 'country', 'bdate', 'sex'])['response']
-    print(friends)
 
-    data = friends
-    dump_report(data)
+    dump_report(friends, save_path, file_format)
 
 
 if __name__ == '__main__':
